a820eeb0c00a303ce7af0ad5681248e657fba6d3
LorraneAP32 <lorranealvespassos@gmail.com>
2025-09-03 11:10:46 -0300

    Exercicio03

---
+++ aula09/exe03.py
@@ -0,0 +1,22 @@
+acessos = [
+    ("Pedro", "sucesso"),
+    ("Ana", "falha"),
+    ("Maria", "sucesso"),
+    ("Pedro", "falha"),
+    ("Ana", "falha")
+]
+
+usuarios_sucesso = set()
+usuarios_falha = set()
+
+for usuario, status in acessos:
+    if status == "sucesso":
+        usuarios_sucesso.add(usuario)
+    else:
+        usuarios_falha.add(usuario)
+
+
+usuarios_somente_falha = usuarios_falha - usuarios_sucesso
+
+print("Usuários com pelo menos um login bem-sucedido:", usuarios_sucesso)
+print("Usuários que tiveram somente logins com falha:", usuarios_somente_falha)
