30a1b447e6ed5cb1d34f783fe614aaeb35e50d10
LorraneAP32 <lorranealvespassos@gmail.com>
2025-09-10 11:57:30 -0300

    aula10-09

---
+++ aula09/aula12/funcaodef.py
@@ -0,0 +1,17 @@
+def diga_oi():
+    print('Oi! Tudo bem ?')
+
+diga_oi()
+
+#_________________________
+
+def saudar(nome):
+    print(f'Olá, {nome}!')
+saudar('Ana')
+saudar('Joao')
+#___________________________
+
+def somar(a, b):
+    return a + b
+total = somar(5, 3)
+print(total*2)
