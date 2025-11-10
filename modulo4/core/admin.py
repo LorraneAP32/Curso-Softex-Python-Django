4a6b96c291dc720eb50e458906dff1c513c4aa30
LorraneAP32 <218288538+LorraneAP32@users.noreply.github.com>
2025-11-10 11:41:05 -0300

    update

--- modulo4/core/admin.py
+++ modulo4/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tarefa 
+from .models import Execucao
+
+admin.site.register(Tarefa)
+admin.site.register(Execucao)
+
 
 # Register your models here.
--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tarefa, Execucao
 # Create your views here.
 def home(request):
+    todas_as_tarefas = Tarefa.objects.all()
+    todas_as_execucoes = Execucao.objects.all()
     context={
         'nome_usuario':'Lorrane',
-        'tecnologias':['python', 'Django','HTML','CSS']
+        'tecnologias':['python', 'Django','HTML','CSS'],
+        'tarefas':todas_as_tarefas,
+        'execucao':todas_as_execucoes
     }
     return render(request, 'home.html',context)
   
