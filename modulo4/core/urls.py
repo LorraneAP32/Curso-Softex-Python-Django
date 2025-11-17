7a2f39726d4914b2d8b3105bf62c7e367fb532d8
LorraneAP32 <218288538+LorraneAP32@users.noreply.github.com>
2025-11-17 11:17:13 -0300

    views

--- modulo4/core/urls.py
+++ modulo4/core/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
         '',
         views.home,
         name='home',),
-    
+    # <int:pk> captura um inteiro da URL e o passa para a view como um argumento chamado 'pk'
+    path('tarefa/<int:pk>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
+    # Ex: /tarefa/5/deletar/
+    path('tarefa/<int:pk>/deletar/', views.deletar_tarefa, name='deletar_tarefa'),
+
     path(
         'site/',
         views.site,
